0a7c6400c56697ff64ba0cb9709a59946da1ad68
Kim, Soyeon <smfqha26@gmail.com>
2020-05-04 10:00:57 +0900

    3차 push

--- midterm/mid4.js
+++ midterm/mid4.js
@@ -1,14 +1,15 @@
+let num=[];
+
 function sum(...a) {
     let result = 0;
     for (let i = 0; i < a.length; ++i)
         result += a[i];
     return result; 
 }
-let a=[];
-let sum=0;
-for(let i=0; i<10; i++){
-    a[i]=Math.floor(Math.random()*100+1);
-    sum+=a[i];
-    
+function test_sum(f){
+    for(let i=0; i<5; i++){
+        num[i]=Math.floor(Math.random()*10)+1;
+        console.log(num);
+    }
 }
-con
+test_sum
